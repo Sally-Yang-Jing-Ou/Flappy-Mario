119c4061cfa2ebaa49e08f37a777bde5b13234a9
Sally Ouyang <sally.ouyang@pulseenergy.com>
2014-07-05 21:53:07 -0700

    Renaming here and there

---
+++ Flappy-Mario/Flappy-Mario/Over.h
@@ -0,0 +1,13 @@
+//
+//  Over.h
+//  Flappy-Mario
+//
+//  Created by Sally Ouyang on 2014-07-05.
+//  Copyright (c) 2014 Sally Ouyang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Over : NSObject
++ (void) over:(UIView*) mainBack :(UILabel *) scoreLabel :(NSInteger) score;
+@end
---
+++ Flappy-Mario/Flappy-Mario/Over.m
@@ -0,0 +1,30 @@
+//
+//  Over.m
+//  Flappy-Mario
+//
+//  Created by Sally Ouyang on 2014-07-05.
+//  Copyright (c) 2014 Sally Ouyang. All rights reserved.
+//
+
+#import "Over.h"
+#import "Score.h"
+
+@implementation Over
++ (void) over:(UIView*) mainBack :(UILabel *)scoreLabel :(NSInteger) score
+{
+    UILabel *best;
+    UIImageView *over = [[UIImageView alloc] initWithFrame:CGRectMake(2, 2, 318, 480)];
+    over.image = [UIImage imageNamed:@"pinkCloud"];
+    [mainBack addSubview:over];
+    best = [[UILabel alloc] initWithFrame:CGRectMake(-25, 300, 300, 30)];
+    best.textAlignment = NSTextAlignmentRight;
+    best.textColor=[UIColor colorWithRed:(188/255.f) green:149 blue:170 alpha:1.0];
+    best.font = [UIFont boldSystemFontOfSize:30];
+    
+    scoreLabel.frame = CGRectMake(-30, 140, 300, 30);
+    scoreLabel.text = [NSString stringWithFormat:@"Current Score: %d",score];
+    [mainBack addSubview:scoreLabel];
+    [mainBack addSubview:best];
+    best.text = [NSString stringWithFormat:@"Best Score: %ld",(long)[Score bestScore]];
+}
+@end
